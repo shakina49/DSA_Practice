b0f98f8e7c1ead7eda0012a39cf8e301eafad322
shakina49 <dasarishakina@gmail.com>
2025-12-16 22:24:36 +0530

    Create power_recursion.java

---
+++ java/Recursion/power_recursion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PowerRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base: ");
+        int base = sc.nextInt();
+        System.out.print("Enter exponent: ");
+        int exp = sc.nextInt();
+        System.out.println(base + "^" + exp + " = " + power(base, exp));
+    }
+
+    public static int power(int base, int exp) {
+        if (exp == 0) return 1;
+        return base * power(base, exp - 1);
+    }
+}
