d18336c23e3ed7dbd3f507955c42973d5e23d361
shakina49 <dasarishakina@gmail.com>
2025-12-16 21:40:03 +0530

    Refactor max element program to accept user input

--- java/arrays/max_element.java
+++ java/arrays/max_element.java
@@ -1,20 +1,23 @@
+import java.util.Scanner;
+
 public class MaxElement {
     public static void main(String[] args) {
-        int[] arr = {3, 7, 1, 9, 4};
+        Scanner sc = new Scanner(System.in);
 
-        if (arr.length == 0) {
-            System.out.println("Array is empty!");
-            return;
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        int max = arr[0]; // assume first element is max
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i]; // update max if current element is bigger
-            }
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) max = arr[i];
         }
 
-        System.out.println("Maximum element is: " + max);
+        System.out.println("Maximum element: " + max);
     }
 }
