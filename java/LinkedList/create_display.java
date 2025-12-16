31c191ee9a5cc62d73db447a161453100783c115
shakina49 <dasarishakina@gmail.com>
2025-12-16 22:07:05 +0530

    Create create_display.java

---
+++ java/LinkedList/create_display.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> list = new LinkedList<>();
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        System.out.println("LinkedList elements: " + list);
+    }
+}
